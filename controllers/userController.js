a5a2e217672798e10f5eca6514d2e70abf3a239d
krupali <krupali.igenerate@gmail.com>
2022-03-24 17:35:21 +0530

    concents given by user api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model.js");
+const Consent = require("../models/consent.model.js");
 const jwt = require("jsonwebtoken");
 const { ObjectId } = require("mongodb");
 
@@ -14,9 +15,11 @@ exports.RegisterUser = async (request, response) => {
     });
 
     await data.save();
-    response.status(200).send({success: true, message:"Registration Successfull!"});
+    response
+      .status(200)
+      .send({ success: true, message: "Registration Successfull!" });
   } catch (err) {
-    response.status(400).send({success: false, message: err.message});
+    response.status(400).send({ success: false, message: err.message });
   }
 };
 
@@ -27,23 +30,56 @@ exports.LoginUser = async (request, response) => {
     if (data) {
       const payload = { id: data._id, username: username };
       const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
-      response.status(400).send({success: true, message:jwtToken});
+      response.status(400).send({ success: true, message: jwtToken });
     } else {
-      response.status(200).send({success: false, message:"Invalid Credentials!"});
+      response
+        .status(200)
+        .send({ success: false, message: "Invalid Credentials!" });
     }
   } catch (err) {
-    response.status(400).send({success: false, message:err.message});
+    response.status(400).send({ success: false, message: err.message });
   }
 };
 
 exports.ChangePassword = async (request, response) => {
   try {
-    const { id } = request.params;
+    console.log(request.data);
+    const { _id } = request.data;
+    console.log(_id);
     const updatedData = request.body;
 
-    const data = await User.findByIdAndUpdate(ObjectId(id), updatedData);
-    response.status(200).send({success: true, message:"Password changed successfully!"});
+    const data = await User.findByIdAndUpdate(_id, updatedData);
+    response
+      .status(200)
+      .send({ success: true, message: "Password changed successfully!" });
   } catch (err) {
-    response.status(400).send({success: false, message:err.message});
+    response.status(400).send({ success: false, message: err.message });
+  }
+};
+
+exports.UserDetail = async (request, response) => {
+  try {
+    const { _id } = request.data;
+    console.log(_id);
+
+    const data = await User.aggregate([
+      {
+        $match: {
+          _id: _id,
+        },
+      },
+      {
+        $lookup: {
+          from: "consents",
+          localField: "_id",
+          foreignField: "createdBy",
+          as: "consentGivenByUser",
+        },
+      },
+    ]);
+
+    response.send(data);
+  } catch (err) {
+    response.status(401).send(err.message);
   }
 };
