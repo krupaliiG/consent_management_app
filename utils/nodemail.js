7ab78742b94678e5cfb3a237e7fe84ff6f74d9cc
krupali <krupali.igenerate@gmail.com>
2022-04-13 19:19:47 +0530

    implemented some file according to folder structure

--- utils/nodemail.js
+++ utils/nodemail.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const port = process.env.email_port;
@@ -6,8 +6,8 @@ const host = process.env.email_host;
 const user = process.env.email_user;
 const pass = process.env.email_pass;
 
-console.log("port", port);
-console.log("host", host);
+// console.log("port", port);
+// console.log("host", host);
 
 const transporter = nodemailer.createTransport({
   port: port,
